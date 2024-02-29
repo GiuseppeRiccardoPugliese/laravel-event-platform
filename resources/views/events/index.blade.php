a08f25024e9a75fd6e1da8c9b996eae2fec68448
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-29 18:32:44 +0100

    Comment for show route

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -7,8 +7,11 @@
     <ul>
         @foreach ($events as $event)
             <li>
-                
-                <a href="{{route('event.show', $event->id)}}"><h2>{{ $event->title }}</h2></a>
+
+                {{-- SHOW --}}
+                <a href="{{ route('event.show', $event->id) }}">
+                    <h2>{{ $event->title }}</h2>
+                </a>
             </li>
         @endforeach
     </ul>
