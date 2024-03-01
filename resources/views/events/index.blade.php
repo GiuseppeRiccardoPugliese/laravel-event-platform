932f09a4154b60d9783c412924f1447df4dac78e
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-03-01 15:25:18 +0100

    Visualizzazione del count per gli eventi associati al singolo utente

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.app')
 @section('content')
-@auth
-    <h1>Events: [{{ count($events) }}]</h1>
-    {{-- CREATE --}}
-    <a href="{{ route('event.create') }}">CREATE</a>
+    @auth
+        <h1 class="text-center">Your events: [{{ count(Auth::user()->events) }}]</h1>
 
-    <ul>
-        @foreach ($events as $event)
-            @if ($user==$event->user->id)
-                <li>
-                    {{-- SHOW --}}
-                    <a href="{{ route('event.show', $event->id) }}">
-                        <h2>{{ $event->title }}</h2>
-                    </a>
-                    <form action="{{ route('event.delete', $event->id) }}" method="POST">
+        {{-- CREATE --}}
+        <a href="{{ route('event.create') }}">CREATE</a>
 
-                        @csrf
-                        @method('DELETE')
+        <ul>
+            @foreach ($events as $event)
+                @if ($user == $event->user->id)
+                    <li>
+                        {{-- SHOW --}}
+                        <a href="{{ route('event.show', $event->id) }}">
+                            <h2>{{ $event->title }}</h2>
+                        </a>
+                        <form action="{{ route('event.delete', $event->id) }}" method="POST">
 
-                        <input type="submit" value="DELETE" id="delete" name="delete">
-                    </form>
-                </li>
-                <a href="{{ route('event.edit', $event->id) }}"><button>EDIT</button></a>
-            @endif
-        @endforeach
+                            @csrf
+                            @method('DELETE')
 
-    </ul>
-@endauth
+                            <input type="submit" value="DELETE" id="delete" name="delete">
+                        </form>
+                    </li>
+                    <a href="{{ route('event.edit', $event->id) }}"><button>EDIT</button></a>
+                @endif
+            @endforeach
 
+        </ul>
+    @endauth
 @endsection
